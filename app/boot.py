89ddc1170fe95cce17fdc007589d9852e0238667
Andres Krogh <andres@Andress-Mac-mini.local>
2022-05-30 17:04:47 -0700

    tinkering

---
+++ app/boot.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import time
+import bmp280 import BPM280
+
+try:
+    from smbus2 import SMBus
+except:
+    from smbus import SMBus
+
+print ("""temperature-and-pressure.py - Displays the temperature and pressure.
+
+Press Ctrl+C to exit!
+
+""")
+
+#Initialise the BMP280
+bus = SMBus(1)
+bmp280 = BMP280(i2c_dev=bus)
+
+while True:
+    temperature = bmp280.get_temperature()
+    pressure = bmp280.get_pressure()
+    print('{:05.2f}*C {:05.2f}hPa'.format(temperature, pressure))
+    time.sleep(1)
