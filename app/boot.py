5fb73f457282ca43c657dbefbce979f287bcb632
Andres Krogh <andres@Andress-Mac-mini.local>
2022-05-30 17:11:41 -0700

    tinkering

--- app/boot.py
+++ app/boot.py
@@ -1,25 +1,9 @@
 #!/usr/bin/env python3
 
-import time
-from bmp280 import BPM280
+import adafruit_bmp280
+i2c = board.I2C()
 
-try:
-    from smbus2 import SMBus
-except:
-    from smbus import SMBus
+sensor = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
 
-print ("""temperature-and-pressure.py - Displays the temperature and pressure.
-
-Press Ctrl+C to exit!
-
-""")
-
-#Initialise the BMP280
-bus = SMBus(1)
-bmp280 = BMP280(i2c_dev=bus)
-
-while True:
-    temperature = bmp280.get_temperature()
-    pressure = bmp280.get_pressure()
-    print('{:05.2f}*C {:05.2f}hPa'.format(temperature, pressure))
-    time.sleep(1)
+print('Temperature: {} degrees C'.format(sensor.temperature))
+print('Pressure: {}hPa'.format(sensor.pressure))
