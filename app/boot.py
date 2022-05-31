183bd7222b41b8d65892974d364f60c8da495497
Andres Krogh <andres@Andress-Mac-mini.local>
2022-05-30 18:10:08 -0700

    tinkering

--- app/boot.py
+++ app/boot.py
@@ -1,22 +1,36 @@
+# import time
+# import board
+# # import digitalio # For use with SPI
+# import adafruit_bmp280
+#
+# # Create sensor object, communicating over the board's default I2C bus
+# i2c = board.I2C()   # uses board.SCL and board.SDA
+# bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
+#
+# # OR Create sensor object, communicating over the board's default SPI bus
+# # spi = board.SPI()
+# # bmp_cs = digitalio.DigitalInOut(board.D10)
+# # bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
+#
+# # change this to match the location's pressure (hPa) at sea level
+# bmp280.sea_level_pressure = 1013.25
+#
+# while True:
+#     print("\nTemperature: %0.1f C" % bmp280.temperature)
+#     print("Pressure: %0.1f hPa" % bmp280.pressure)
+#     print("Altitude = %0.2f meters" % bmp280.altitude)
+#     time.sleep(2)
+
+
 import time
 import board
-# import digitalio # For use with SPI
-import adafruit_bmp280
+import adafruit_ahtx0
 
 # Create sensor object, communicating over the board's default I2C bus
-i2c = board.I2C()   # uses board.SCL and board.SDA
-bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c, address=0x38)
-
-# OR Create sensor object, communicating over the board's default SPI bus
-# spi = board.SPI()
-# bmp_cs = digitalio.DigitalInOut(board.D10)
-# bmp280 = adafruit_bmp280.Adafruit_BMP280_SPI(spi, bmp_cs)
-
-# change this to match the location's pressure (hPa) at sea level
-bmp280.sea_level_pressure = 1013.25
+i2c = board.I2C()  # uses board.SCL and board.SDA
+sensor = adafruit_ahtx0.AHTx0(i2c)
 
 while True:
-    print("\nTemperature: %0.1f C" % bmp280.temperature)
-    print("Pressure: %0.1f hPa" % bmp280.pressure)
-    print("Altitude = %0.2f meters" % bmp280.altitude)
+    print("\nTemperature: %0.1f C" % sensor.temperature)
+    print("Humidity: %0.1f %%" % sensor.relative_humidity)
     time.sleep(2)
