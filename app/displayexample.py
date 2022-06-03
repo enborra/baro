36e96fc5bec068ce11be3f7e18c00020574411ce
Andres Krogh <andres@Andress-Mac-mini.local>
2022-06-03 14:36:35 -0700

    .

--- app/displayexample.py
+++ app/displayexample.py
@@ -64,6 +64,6 @@ while True:
     text_area = label.Label(terminalio.FONT, text=text)
     text_area.x = 10
     text_area.y = 10
-    board.DISPLAY.show(text_area)
+    display.show(text_area)
 
     time.sleep(2)
