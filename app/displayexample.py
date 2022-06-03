03776c4419be9130deed7434824e7ef5eb68ba5a
Andres Krogh <andres@Andress-Mac-mini.local>
2022-06-03 14:47:49 -0700

    .

--- app/displayexample.py
+++ app/displayexample.py
@@ -40,7 +40,7 @@ import digitalio
 import board
 from adafruit_rgb_display import color565
 # import adafruit_rgb_display.ili9341 as ili9341
-import adafruit_ili9341
+import Adafruit_ILI9341
 import terminalio
 import displayio
 
