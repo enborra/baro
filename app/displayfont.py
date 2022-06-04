7381f82a7022e9874764ed29ab5388cccf9c48cf
Andres Krogh <andres@Andress-Mac-mini.local>
2022-06-03 17:30:33 -0700

    .

--- app/displayfont.py
+++ app/displayfont.py
@@ -49,8 +49,8 @@ color = 0xFFFFFF
 text_area = label.Label(font, text=text, color=color)
 
 # Set the location
-text_area.x = 80
-text_area.y = 120
+text_area.x = 90
+text_area.y = 110
 text_area.scale = 1
 
 # Show it
