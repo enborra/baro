c4d01ae926d589a176732fd118e5a7a99fb0a07e
Andres Krogh <andres@Andress-Mac-mini.local>
2022-06-03 17:07:29 -0700

    .

--- app/displayexample.py
+++ app/displayexample.py
@@ -1,66 +1,16 @@
-# import time
-# import busio
-# import digitalio
-# # from board import SCK, MOSI, MISO, D2, D3
-# from board import SCK, MOSI, MISO, CE0, D24, D25
-#
-# from adafruit_rgb_display import color565
-# import adafruit_rgb_display.ili9341 as ili9341
-#
-#
-# # Configuration for CS and DC pins:
-# # CS_PIN = D2
-# # DC_PIN = D3
-# CS_PIN = CE0
-# DC_PIN = D25
-#
-# # Setup SPI bus using hardware SPI:
-# spi = busio.SPI(clock=SCK, MOSI=MOSI, MISO=MISO)
-#
-# # Create the ILI9341 display:
-# display = ili9341.ILI9341(spi, cs=digitalio.DigitalInOut(CS_PIN),
-#                           dc=digitalio.DigitalInOut(DC_PIN))
-#
-# # Main loop:
-# while True:
-#     # Clear the display
-#     display.fill(0)
-#     # Draw a red pixel in the center.
-#     display.pixel(0, 0, color565(255, 0, 0))
-#     # Pause 2 seconds.
-#     time.sleep(2)
-#     # Clear the screen blue.
-#     display.fill(color565(0, 0, 255))
-#     # Pause 2 seconds.
-#     time.sleep(2)
-
 import time
 import busio
 import digitalio
 import board
-from adafruit_rgb_display import color565
-# import adafruit_rgb_display.ili9341 as ili9341
-import adafruit_ili9341
 import terminalio
 import displayio
+
+from adafruit_rgb_display import color565
+import adafruit_ili9341
 from adafruit_display_text import label
 
 
 displayio.release_displays()
-
-# spi = busio.SPI(
-#     clock=board.SCK,
-#     MOSI=board.MOSI,
-#     MISO=board.MISO
-# )
-
-# display = Adafruit_ILI9341.ILI9341(
-#     spi,
-#     cs=digitalio.DigitalInOut(board.CE0),
-#     dc=digitalio.DigitalInOut(board.D25),
-#     width=320,
-#     height=240
-# )
 
 spi = board.SPI()
 tft_cs = board.CE0
@@ -82,18 +32,7 @@ display = adafruit_ili9341.ILI9341(
 splash = displayio.Group()
 display.show(splash)
 
-
 while True:
-    # display.fill(color565(0x00, 0x00, 0x00))
-    # display.fill_circle(100, 120, 20, color565(0xff, 0xff, 0xff))
-    # display.fill_circle(100, 220, 20, color565(0xff, 0xff, 0xff))
-    #
-    # time.sleep(2)
-    #
-    # display.fill(color565(0x00, 0x00, 0x00))
-
-
-
     text = "Hello world"
     text_area = label.Label(
         terminalio.FONT,
@@ -104,6 +43,5 @@ while True:
     text_area.y = 10
 
     splash.append(text_area)
-    # display.show(my_display_group)
 
     time.sleep(2)
---
+++ app/displayfont.py
@@ -0,0 +1,85 @@
+import board
+import displayio
+
+import adafruit_ili9341
+from adafruit_rgb_display import color565
+from adafruit_display_text import label
+from adafruit_bitmap_font import bitmap_font
+
+displayio.release_displays()
+
+spi = board.SPI()
+tft_cs = board.CE0
+tft_dc = board.D25
+
+display_bus = displayio.FourWire(
+    spi,
+    command=tft_dc,
+    chip_select=tft_cs,
+    reset=board.D6
+)
+
+display = adafruit_ili9341.ILI9341(
+    display_bus,
+    width=320,
+    height=240
+)
+
+splash = displayio.Group()
+display.show(splash)
+
+
+font_file = "futura-medium-72.ttc"
+
+font = bitmap_font.load_font(font_file)
+
+bitmap = displayio.Bitmap(display.width, display.height, 2)
+
+palette = displayio.Palette(2)
+
+palette[0] = 0x000000
+palette[1] = 0xFFFFFF
+
+_, height, _, dy = font.get_bounding_box()
+for y in range(height):
+    pixels = []
+    for c in "Adafruit CircuitPython":
+        glyph = font.get_glyph(ord(c))
+        if not glyph:
+            continue
+        glyph_y = y + (glyph.height - (height + dy)) + glyph.dy
+
+        if 0 <= glyph_y < glyph.height:
+            for i in range(glyph.width):
+                value = glyph.bitmap[i, glyph_y]
+                pixel = 0
+                if value > 0:
+                    pixel = 1
+                pixels.append(pixel)
+        else:
+            # empty section for this glyph
+            for i in range(glyph.width):
+                pixels.append(0)
+
+        # one space between glyph
+        pixels.append(0)
+
+    if pixels:
+        for x, pixel in enumerate(pixels):
+            bitmap[x, y] = pixel
+
+# Create a TileGrid to hold the bitmap
+tile_grid = displayio.TileGrid(bitmap, pixel_shader=palette)
+
+# Create a Group to hold the TileGrid
+group = displayio.Group()
+
+group.x = 20
+# Add the TileGrid to the Group
+group.append(tile_grid)
+
+# Add the Group to the Display
+display.show(group)
+
+while True:
+    pass
