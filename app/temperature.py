8c2712bc519f915bd169160e4a818e2c8c390ea4
Andres Krogh <andres@Andress-Mac-mini.local>
2022-06-03 21:19:35 -0700

    .

--- app/temperature.py
+++ app/temperature.py
@@ -43,7 +43,7 @@ font_large = bitmap_font.load_font("fonts/futura-medium-35.bdf")
 font_small = bitmap_font.load_font("fonts/futura-medium-20.bdf")
 
 while True:
-    display.drawRect(0,0,width,height, color565(0xff,0xff,0xff))
+    display.clearDisplay()
 
     temp = "Temp: %d°F" % ((sensor.temperature*1.8)+32)
     humidity = "Humidity: %d" % sensor.relative_humidity
