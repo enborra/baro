9c548ff56a5d7e1f1a6443c6d9f69e3e3cf4976f
Andres Krogh <andres@Andress-Mac-mini.local>
2022-07-02 13:45:29 -0700

    .

--- app/boot.py
+++ app/boot.py
@@ -13,7 +13,7 @@ import calendar
 # from adafruit_bitmap_font import bitmap_font
 
 from models.Detector import Detector
-from models.HeadsUpDisplay import HeadsUpDisplay
+# from models.HeadsUpDisplay import HeadsUpDisplay
 
 
 def keyboardHandler(signum, frame):
