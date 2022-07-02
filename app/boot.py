fbf6731f74f119ca880ad46a6d459e79fcf7315c
Andres Krogh <andres@Andress-Mac-mini.local>
2022-07-02 13:36:12 -0700

    .

--- app/boot.py
+++ app/boot.py
@@ -13,6 +13,7 @@ from adafruit_display_text import label
 from adafruit_bitmap_font import bitmap_font
 
 from models.Detector import Detector
+from models.Hud import Hud
 
 
 def keyboardHandler(signum, frame):
@@ -24,41 +25,44 @@ if __name__ == "__main__":
     d = Detector()
     d.refresh()
 
-    displayio.release_displays()
+    hud = Hud()
+    hud.show()
 
-    spi = board.SPI()
-    tft_cs = board.CE0
-    tft_dc = board.D25
+    # displayio.release_displays()
+    #
+    # spi = board.SPI()
+    # tft_cs = board.CE0
+    # tft_dc = board.D25
+    #
+    # display_bus = displayio.FourWire( spi, command=tft_dc, chip_select=tft_cs, reset=board.D6 )
+    # display = adafruit_ili9341.ILI9341( display_bus, width=320, height=240, rotation=180 )
+    # splash = displayio.Group()
+    #
+    # # Blank the screen
+    #
+    # color_bitmap = displayio.Bitmap(320, 240, 1)
+    # color_palette = displayio.Palette(1)
+    # color_palette[0] = 0x000000
+    # bg_sprite = displayio.TileGrid(color_bitmap, x=0, y=0, pixel_shader=color_palette)
+    # splash.append(bg_sprite)
+    #
+    # display.show(splash)
 
-    display_bus = displayio.FourWire( spi, command=tft_dc, chip_select=tft_cs, reset=board.D6 )
-    display = adafruit_ili9341.ILI9341( display_bus, width=320, height=240, rotation=180 )
-    splash = displayio.Group()
-
-    # Blank the screen
-
-    color_bitmap = displayio.Bitmap(320, 240, 1)
-    color_palette = displayio.Palette(1)
-    color_palette[0] = 0x000000
-    bg_sprite = displayio.TileGrid(color_bitmap, x=0, y=0, pixel_shader=color_palette)
-    splash.append(bg_sprite)
-
-    display.show(splash)
-
-    font_large = bitmap_font.load_font("fonts/futura-medium-35.bdf")
-    font_small = bitmap_font.load_font("fonts/futura-medium-20.bdf")
+    # font_large = bitmap_font.load_font("fonts/futura-medium-35.bdf")
+    # font_small = bitmap_font.load_font("fonts/futura-medium-20.bdf")
     cycle_count = 0
     displayFace = 0
 
-    # Create the ui components
-
-    txtTitle = label.Label( font_large, text='', color=0xffffff, x=40, y=70, scale=1 )
-    splash.append( txtTitle )
-
-    txtBody = label.Label( font_small, text='', color=0xffffff, x=40, y=130, scale=1 )
-    splash.append( txtBody )
-
-    txtTime = label.Label( font_small, text='', color=0xaaaaaa, x=15, y=15, scale=1 )
-    splash.append( txtTime )
+    # # Create the ui components
+    #
+    # txtTitle = label.Label( font_large, text='', color=0xffffff, x=40, y=70, scale=1 )
+    # splash.append( txtTitle )
+    #
+    # txtBody = label.Label( font_small, text='', color=0xffffff, x=40, y=130, scale=1 )
+    # splash.append( txtBody )
+    #
+    # txtTime = label.Label( font_small, text='', color=0xaaaaaa, x=15, y=15, scale=1 )
+    # splash.append( txtTime )
 
     while True:
         time.sleep(3)
@@ -85,9 +89,11 @@ if __name__ == "__main__":
             else:
                 displayFace = 0
 
-            txtTime.text = strftime("%H:%M", time.localtime())
-            txtTitle.text = title_text
-            txtBody.text = body_text
+            hud.setText( title=title_text, body=body_text )
+
+            # txtTime.text = strftime("%H:%M", time.localtime())
+            # txtTitle.text = title_text
+            # txtBody.text = body_text
 
             cycle_count = cycle_count + 1
 
---
+++ app/models/hud.py
@@ -0,0 +1,65 @@
+import board
+import displayio
+import adafruit_ili9341
+from adafruit_rgb_display import color565
+from adafruit_display_text import label
+from adafruit_bitmap_font import bitmap_font
+
+
+class Hud():
+    _spi = None
+    _tft_cs = board.CE0
+    _tft_dc = board.D25
+    _tft_reset = board.D6
+    _displayBus = None
+    _displayWidth = 320
+    _displayHeight = 240
+    _displayRotation = 180
+    _display = None
+    _splash = None
+    _bg_sprite = None
+    _fontLarge = None
+    _fontSmall = None
+    _txtTime = None
+    _txtTitle = None
+    _txtBody = None
+
+
+    def __init__(self, *args, **kwargs):
+        displayio.release_displays()
+
+        self._displayBus = displayio.FourWire( self._spi, command=self._tft_dc, chip_select=self._tft_cs, reset=self._tft_reset )
+        self._display = adafruit_ili9341.ILI9341( self._display_bus, width=self._displayWidth, height=self._displayHeight, rotation=self._displayRotation )
+        self._splash = displayio.Group()
+
+        # Blank the screen
+
+        color_bitmap = displayio.Bitmap(320, 240, 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0x000000
+
+        self._bg_sprite = displayio.TileGrid(color_bitmap, x=0, y=0, pixel_shader=color_palette)
+        self._splash.append(bg_sprite)
+        self._display.show(splash)
+
+        _fontLarge = bitmap_font.load_font("fonts/futura-medium-35.bdf")
+        _fontSmall = bitmap_font.load_font("fonts/futura-medium-20.bdf")
+
+        # Create the ui components
+
+        self._txtTitle = label.Label( font_large, text='', color=0xffffff, x=40, y=70, scale=1 )
+        splash.append( self._txtTitle )
+
+        self._txtBody = label.Label( font_small, text='', color=0xffffff, x=40, y=130, scale=1 )
+        splash.append( self._txtBody )
+
+        self._txtTime = label.Label( font_small, text='', color=0xaaaaaa, x=15, y=15, scale=1 )
+        splash.append( self._txtTime )
+
+    def setText(self, *args, **kwargs):
+        title_text = kwargs.get('title')
+        body_text = kwargs.get('body')
+
+        self._txtTime.text = strftime("%H:%M", time.localtime())
+        self._txtTitle.text = title_text
+        self._txtBody.text = body_text
