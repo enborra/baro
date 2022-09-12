b5e6b069c3906835662c92d2f69cc1ae015a3738
Andres Krogh <andres@Andress-Mac-mini.local>
2022-09-11 23:44:41 -0700

    .

--- app/models/Broadcaster.py
+++ app/models/Broadcaster.py
@@ -2,17 +2,19 @@ import requests
 
 
 class Broadcaster():
-
+    _broadcast_interval = 10
+    _interval_count = 0
 
 
     def __init__(self, *args, **kwargs):
-
-
         pass
 
     def cycle(self, *args, **kwargs):
-        # t = kwargs['temp']
-        t = '0'
 
-        r = requests.get('http://admin.gingerbee.co/api/temp?t='+t+'&h=3&b=2')
-        # pass
+        if self._interval_count > self._broadcast_interval:
+            t = str(kwargs['temp'])
+            # t = '0'
+
+            r = requests.get('http://admin.gingerbee.co/api/temp?t='+t+'&h=3&b=2')
+
+            self._interval_count += 1
